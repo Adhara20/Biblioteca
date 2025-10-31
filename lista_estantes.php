265e7c77c7c62fb90b138486a2aba872d2259378
Sergio <sergiotomascontrerasdiaz@gmail.com>
2025-10-31 09:52:17 -0700

    Reparando un error

--- lista_estantes.php
+++ lista_estantes.php
@@ -1,33 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
 </head>
+
 <body>
-    <h2>lista Estantes:</h2>
-    	<?php 
-include('clases/estantes.php');
-$clase = new Estantes();
-$resultado = $clase->mostrar();
-?>
-<table border="1" cellpadding="5" >
-        <th>id</th>
+	<h2>lista Estantes:</h2>
+	<?php
+	include('clases/estantes.php');
+	$clase = new Estantes();
+	$resultado = $clase->mostrar();
+	?>
+	<table border="1" cellpadding="5">
+		<th>id</th>
 		<th>Nombre</th>
 		<th>Acciones</th>
-	</tr>
-	<?php
-	foreach ($resultado as $fila):
-		?>
-		<tr>
-            <td><?=$fila["pkEstante"]?></td>
-			<td><?=$fila["nivel"]?></td>
-			<td>(proximamente...)</td>
 		</tr>
-	<?php endforeach; 
-    ?>
-</table>
-    
+		<?php
+		foreach ($resultado as $fila):
+		?>
+			<tr>
+				<td><?= $fila["pkEstante"] ?></td>
+				<td><?= $fila["nivel"] ?></td>
+				<td>(proximamente...)</td>
+			</tr>
+		<?php endforeach;
+		?>
+	</table>
+
 </body>
+
 </html>
+<!-- Holi -->
