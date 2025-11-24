e775f7000fa37b68f4187fbb9149acce71a79f03
Adhara <adhara0620@gmail.com>
2025-11-24 07:39:43 -0700

    Eliminar archivo filtrar_estanterias.php

--- controladores/filtrar_estanterias.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('../clases/estanterias.php');
-$clase = new Estanterias();
-
-$buscar = $_GET['buscar'] ?? '';
-$estatus = $_GET['estatus'] ?? '';
-
-$resultado = $clase->filtrar($buscar, $estatus);
-?>
