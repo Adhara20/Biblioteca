8b994a1fd13820f4c92a141c451c5c3534e2b4b4
Sergio <sergiotomascontrerasdiaz@gmail.com>
2025-12-02 08:20:51 -0700

    Asteriscos

--- vistas/formulario_usuario.php
+++ vistas/formulario_usuario.php
@@ -38,13 +38,13 @@ requireRole(['A', 'B']);
       <form action="../controladores/insertar_usuario.php" method="POST" enctype="multipart/form-data" class="grid grid-cols-1 md:grid-cols-2 gap-4">
 
         <div>
-          <label class="block text-sm font-medium text-gray-700">Nombre(s)</label>
+          <label class="block text-sm font-medium text-gray-700">Nombre(s)<span class="text-red-500 text-2xl">*</span></label>
           <input type="text" name="nombres" placeholder="Nombre(s)" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087] uppercase" value="<?= $_SESSION['form_usuario']['nombres'] ?? '' ?>">
         </div>
 
         <div>
-          <label class="block text-sm font-medium text-gray-700">Apellido Paterno</label>
+          <label class="block text-sm font-medium text-gray-700">Apellido Paterno<span class="text-red-500 text-2xl">*</span></label>
           <input type="text" name="apaterno" placeholder="Apellido Paterno" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087] uppercase" value="<?= $_SESSION['form_usuario']['apaterno'] ?? '' ?>">
         </div>
@@ -56,19 +56,19 @@ requireRole(['A', 'B']);
         </div>
 
         <div>
-          <label class="block text-sm font-medium text-gray-700">CURP</label>
+          <label class="block text-sm font-medium text-gray-700">CURP<span class="text-red-500 text-2xl">*</span></label>
           <input type="text" name="curp" placeholder="CURP" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087] uppercase" value="<?= $_SESSION['form_usuario']['curp'] ?? '' ?>">  <!-- uppercase (es una clase de Talwing para que lo que escribas se VEA MAYÚSCULAS. Ojo, es solo para que al escribir se vea en MAYÚSCULAS, para que se guarde como Mayuscula se hace en insertar) -->
         </div>
 
         <div>
-          <label class="block text-sm font-medium text-gray-700">Fecha de Nacimiento</label>
+          <label class="block text-sm font-medium text-gray-700">Fecha de Nacimiento<span class="text-red-500 text-2xl">*</span></label>
           <input type="date" name="fechaNac" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087]" value="<?= $_SESSION['form_usuario']['fechaNac'] ?? '' ?>">
         </div>
 
         <div>
-          <label class="block text-sm font-medium text-gray-700">Sexo</label>
+          <label class="block text-sm font-medium text-gray-700">Sexo<span class="text-red-500 text-2xl">*</span></label>
           <select name="sexo" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087] bg-white">
             <option value="M" <?= (isset($_SESSION['form_usuario']['sexo']) && $_SESSION['form_usuario']['sexo'] === 'M') ? 'selected' : '' ?>>Masculino</option>
@@ -77,26 +77,26 @@ requireRole(['A', 'B']);
         </div>
 
         <div class="md:col-span-2">
-          <label class="block text-sm font-medium text-gray-700">Correo Electrónico</label>
+          <label class="block text-sm font-medium text-gray-700">Correo Electrónico<span class="text-red-500 text-2xl">*</span></label>
           <input type="email" name="correo" placeholder="correo@ejemplo.com" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087]" value="<?= $_SESSION['form_usuario']['correo'] ?? '' ?>">
         </div>
 
         <!-- Contraseña -->
         <div>
-          <label class="block text-sm font-medium text-gray-700">Contraseña</label>
+          <label class="block text-sm font-medium text-gray-700">Contraseña<span class="text-red-500 text-2xl">*</span></label>
           <input type="password" name="pass" placeholder="De 8 a 20 caracteres" minlength="8" maxlength="20" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087]">
         </div>
         <!-- Confirmar Contraseñ -->
          <div>
-          <label class="block text-sm font-medium text-gray-700">Confirmar Contraseña</label>
+          <label class="block text-sm font-medium text-gray-700">Confirmar Contraseña<span class="text-red-500 text-2xl">*</span></label>
           <input type="password" name="confirmarPass" placeholder="De 8 a 20 caracteres" minlength="8" maxlength="20" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087]">
         </div>
 
         <div>
-          <label class="block text-sm font-medium text-gray-700">Rol</label>
+          <label class="block text-sm font-medium text-gray-700">Rol<span class="text-red-500 text-2xl">*</span></label>
           <select name="rol" required
             class="w-full mt-1 p-2 border rounded-md focus:outline-[#4F0087] bg-white">
             <option value="L" <?= (isset($_SESSION['form_usuario']['rol']) && $_SESSION['form_usuario']['rol'] === 'L') ? 'selected' : '' ?>>Lector</option>
@@ -106,7 +106,7 @@ requireRole(['A', 'B']);
         </div>
 
         <div class="md:col-span-2">
-          <label class="block text-sm font-medium text-gray-700">Foto de Usuario</label>
+          <label class="block text-sm font-medium text-gray-700">Foto de Usuario<span class="text-red-500 text-2xl">*</span></label>
           <input type="file" name="foto"
             class="w-full mt-1 p-2 border rounded-md bg-white focus:outline-[#4F0087]">
         </div>
