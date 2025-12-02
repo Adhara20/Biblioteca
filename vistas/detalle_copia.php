b60412d91c155adbc19f109baafb6fd1972f5990
Adhara <adhara0620@gmail.com>
2025-12-02 08:39:46 -0700

    Mis cambios finales(Index completo y Validaciones completas)

--- vistas/detalle_copia.php
+++ vistas/detalle_copia.php
@@ -1,3 +1,9 @@
+<?php
+require_once('../includes/auth.php');
+
+// Solo Admin y Bibliotecario
+requireRole(['A', 'B', 'L']);
+?>
 <!-- incluir Header y Menu-->
 <?php 
 include('../includes/header.php');
