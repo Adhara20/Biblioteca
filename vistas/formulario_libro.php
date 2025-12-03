872109410f3ac11d197ccb0784d0c56eb2a62fad
Luis-eduta <luiseduardotoale15@gmail.com>
2025-12-03 13:57:33 -0600

    nuevos cambios boton cancelar libros y prestamos

--- vistas/formulario_libro.php
+++ vistas/formulario_libro.php
@@ -161,10 +161,15 @@ $listaIdiomas = $idioma->mostrar();
     </div>
 
     <!-- BOTONES -->
-    <div class="md:col-span-2 flex flex-col gap-3 md:flex-row md:justify-end mt-4">
+    <!-- <div class="md:col-span-2 flex flex-col gap-3 md:flex-row md:justify-end mt-4">
       <button class="w-full md:w-32 bg-[#B55780] text-white py-2 rounded-md font-semibold hover:bg-[#c46b93] transition">
         Cancelar
-      </button>
+      </button> -->
+        <div class="flex flex-col gap-3 md:flex-row md:justify-end mt-4">
+      <a href="lista_libros.php"
+        class="w-full md:w-32 bg-[#B55780] text-white text-center py-2 rounded-md font-semibold hover:bg-[#c46b93] transition">
+        Cancelar
+        </a>
       <button type="submit"
         class="w-full md:w-32 bg-[#4F0087] text-white py-2 rounded-md font-semibold hover:bg-[#6A00B8] transition">
         Guardar
--- vistas/formulario_prestamo.php
+++ vistas/formulario_prestamo.php
@@ -113,10 +113,15 @@ $folioCopia = $_GET['folio'] ?? null;
     </div>
         
         <!-- BOTONES -->
-    <div class="md:col-span-2 flex flex-col gap-3 md:flex-row md:justify-end mt-4">
+    <!-- <div class="md:col-span-2 flex flex-col gap-3 md:flex-row md:justify-end mt-4">
       <button class="w-full md:w-32 bg-[#B55780] text-white py-2 rounded-md font-semibold hover:bg-[#c46b93] transition">
         Cancelar
-      </button>
+      </button> -->
+         <div class="flex flex-col gap-3 md:flex-row md:justify-end mt-4">
+      <a href="lista_prestamos.php"
+        class="w-full md:w-32 bg-[#B55780] text-white text-center py-2 rounded-md font-semibold hover:bg-[#c46b93] transition">
+        Cancelar
+        </a>
       <button type="submit"
         class="w-full md:w-32 bg-[#4F0087] text-white py-2 rounded-md font-semibold hover:bg-[#6A00B8] transition">
         Guardar
